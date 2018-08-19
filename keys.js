b1b322f179733015617026eae5955848e82262f2
SuzanneRashka <suzanne.rashka@gmail.com>
2018-08-19 11:30:40 -0500

    working on omdb today for redemption

--- keys.js
+++ keys.js
@@ -5,4 +5,6 @@ exports.spotify = {
     secret: process.env.SPOTIFY_SECRET
 }
 // exports.band = {}
-// exports.omdb = {}
+exports.omdb = {
+    key: 'trilogy'
+}
--- liri.js
+++ liri.js
@@ -7,67 +7,56 @@ var keys = require('./keys.js');
 var request = require('request');
 var dotenv = require('dotenv');
 var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
 // var moment = requrire('moment');
 
-var input = process.argv;
-var command = process.argv[2];
-var name = "";
-// for loop to catch more than one word titles
-for (i = 3; i < input.length; i++) {
-    name = name + " " + input[i];
+var userSelection = process.argv[2];
+var mySong = process.argv[3];
+
+if (userSelection === 'spotify-this-song' && mySong) {
+    console.log("success");
+    getSong();
 }
+// defaulting to "The Sign" by Ace of Base if no song is specified
+// else if (userSelection === 'spotify-this-song' && mySong != true) {
+//     console.log("failed");
+//     spotify.search({
+//         type: 'track',
+//         query: 'The Sign'
+//     }, function (err, data) {
+//         if (err) {
+//             return console.log('Error occurred: ' + err);
+//         }
 
-function getMeSpotify(songName) {
-    let spotify = new Spotify(keys.spotify);
-
-    if (!songName) {
-        songName = "Which Way Will You Choose";
-        console.log("You didn't make a selection, so Randy Travis will ask you repeatedly")
-    }
-
-    spotify.search({
-        type: 'track',
-        query: songName
-    }, function (err, data) {
-        if (err) {
-            console.log('Error occurred: ' + err);
-            return;
-        } else {
-            output = space + "================= LIRI FOUND THIS FOR YOU...==================" +
-                space + "Song Name: " + "'" + songName.toUpperCase() + "'" +
-                space + "Album Name: " + data.tracks.items[0].album.name +
-                space + "Artist Name: " + data.tracks.items[0].album.artists[0].name +
-                space + "URL: " + data.tracks.items[0].album.external_urls.spotify + "\n\n\n";
-            console.log(output);
-
-            fs.appendFile("log.txt", output, function (err) {
-                if (err) throw err;
-                console.log('Saved!');
-            });
-        };
-    });
-}
-getMeSpotify(command, input)
-// if (command === 'spotify-this-song') {
-//     var spotify = new Spotify(keys.spotify);
-
-//     if (name === "") {
-//         songName = "Which Way Will You Choose";
-//         console.log("You didn't make a selection, so Randy Travis will ask you repeatedly")
-//     } else {
-//         spotify.search({
-//             type: 'track',
-//             query: songName,
-//             limit: 2
-//         }, function (err, data) {
-//             if (err) {
-//                 return err;
-//             } else {
-//                 console.log(data);
-//             }
-//         })
-//     }
+//         console.log(JSON.stringify(data, null, 2));
+//     });
 // }
+
+// function getSong() {
+//     spotify.search({
+//         type: 'track',
+//         query: mySong
+//     }, function (err, data) {
+//         if (err) {
+//             console.log(err);
+//         }
+//         console.log(JSON.stringify(data, null, 2));
+//     });
+// }
+
+// omdb
+
+
+var movie = "Alpha";
+var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=" + keys.omdb;
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    createRow(response);
+});
+
+
 
 ///  YET TO DO.....
 // function concertThis(){}
@@ -82,3 +71,6 @@ getMeSpotify(command, input)
 //     // var dataArr = data.split(", ");
 //     // console.log(dataArr);
 // })
+
+// songName = "Which Way Will You Choose";
+//         console.log("You didn't make a selection, so Randy Travis will ask you repeatedly")
