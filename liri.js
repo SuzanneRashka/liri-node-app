4aeb341b49424b7269e5db47874d6c06cba2b73b
SuzanneRashka <suzanne.rashka@gmail.com>
2018-08-18 20:49:42 -0500

    nothing

--- liri.js
+++ liri.js
@@ -6,13 +6,68 @@ var fs = require('fs');
 var keys = require('./keys.js');
 var request = require('request');
 var dotenv = require('dotenv');
-var spotify = require('node-spotify-api');
+var Spotify = require('node-spotify-api');
 // var moment = requrire('moment');
 
-spotify = new Spotify(keys.spotify);
+var input = process.argv;
+var command = process.argv[2];
+var name = "";
+// for loop to catch more than one word titles
+for (i = 3; i < input.length; i++) {
+    name = name + " " + input[i];
+}
 
-console.log(spotify);
+function getMeSpotify(songName) {
+    let spotify = new Spotify(keys.spotify);
 
+    if (!songName) {
+        songName = "Which Way Will You Choose";
+        console.log("You didn't make a selection, so Randy Travis will ask you repeatedly")
+    }
+
+    spotify.search({
+        type: 'track',
+        query: songName
+    }, function (err, data) {
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+        } else {
+            output = space + "================= LIRI FOUND THIS FOR YOU...==================" +
+                space + "Song Name: " + "'" + songName.toUpperCase() + "'" +
+                space + "Album Name: " + data.tracks.items[0].album.name +
+                space + "Artist Name: " + data.tracks.items[0].album.artists[0].name +
+                space + "URL: " + data.tracks.items[0].album.external_urls.spotify + "\n\n\n";
+            console.log(output);
+
+            fs.appendFile("log.txt", output, function (err) {
+                if (err) throw err;
+                console.log('Saved!');
+            });
+        };
+    });
+}
+getMeSpotify(command, input)
+// if (command === 'spotify-this-song') {
+//     var spotify = new Spotify(keys.spotify);
+
+//     if (name === "") {
+//         songName = "Which Way Will You Choose";
+//         console.log("You didn't make a selection, so Randy Travis will ask you repeatedly")
+//     } else {
+//         spotify.search({
+//             type: 'track',
+//             query: songName,
+//             limit: 2
+//         }, function (err, data) {
+//             if (err) {
+//                 return err;
+//             } else {
+//                 console.log(data);
+//             }
+//         })
+//     }
+// }
 
 ///  YET TO DO.....
 // function concertThis(){}
